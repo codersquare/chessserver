f878280045e0eeac4592f79c5b7f59a912de7860
Shen Shen <shenshen@Shens-Macbook-Pro.local>
2013-09-02 21:23:39 -0400

    sending out function update

--- src/Handler.java
+++ src/Handler.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 
 public class Handler {
-	public ArrayList<String> array = null;
+/*	public ArrayList<String> array = null;
 	public void save(String inputstream){
 	array.add(inputstream);
 	System.out.println(array);
@@ -12,7 +12,7 @@ public class Handler {
 		/*String from=inputstream.substring(0,2);
 		String to=inputstream.substring(2,4);
 		System.out.println(from);
-		System.out.println(to);*/
+		System.out.println(to);
 	}
 	final public void setGame1(boolean game1){
 		game1=!game1;
@@ -20,5 +20,5 @@ public class Handler {
 	public boolean returnGame1()
 	{
 		return game1;
-	}
+	}*/
 }
--- src/MyServer.java
+++ src/MyServer.java
@@ -3,18 +3,27 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Hashtable;
 
 
 public class MyServer {
+	private Hashtable<String, Socket> userlists;
+	private ServerSocket serverSocket = null;
+	private Socket socket = null;
+	private DataInputStream dataInputStream = null;
+	private DataOutputStream dataOutputStream = null;
+	private Handler handler = new Handler();
+	private boolean game1 = false;
 
-	public static void main(String[] args){
-		ServerSocket serverSocket = null;
-		Socket socket = null;
-		DataInputStream dataInputStream = null;
-		DataOutputStream dataOutputStream = null;
-		Handler handler = new Handler();
-		boolean game1 = false;
+	public MyServer(){
+		System.out.println("Inside startserver ");
+
+	}
+	public void startServer(){
 		try {
+			userlists = new Hashtable<String, Socket>();
+
 			serverSocket = new ServerSocket(8888);
 			System.out.println("Listening :8888");
 		} catch (IOException e) {
@@ -26,28 +35,9 @@ public class MyServer {
 				socket = serverSocket.accept();
 				dataInputStream = new DataInputStream(socket.getInputStream());
 				dataOutputStream = new DataOutputStream(socket.getOutputStream());
-				//String ip=socket.getInetAddress();
+				userlists.put(socket.getInetAddress().toString(),socket);				
 				String receive=dataInputStream.readUTF();
 				System.out.println(receive);
-
-			   // handler.save(receive);
-
-				if(receive.equals("l0")){
-					handler.setGame1();
-					
-				}
-				if(receive.equals("l1")&&game1)
-				{
-					dataOutputStream.writeUTF("hey1");
-				}
-
-
-
-				/*else{
-					handler.save(receive);
-					dataOutputStream.writeUTF("hello");
-					System.out.println("hello");
-				}*/
 
 
 			} catch (IOException e) {
@@ -66,6 +56,7 @@ public class MyServer {
 				
 				if( dataInputStream!= null){
 					try {
+						startSend();
 						dataInputStream.close();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
@@ -82,6 +73,45 @@ public class MyServer {
 					}
 				}
 			}
-		}
+		}		
 	}
+
+	public void startSend(){
+		Socket sendsocket = null;
+		DataOutputStream dataOutputStream = null;
+		DataInputStream dataInputStream = null;
+		try {               
+		sendsocket = new Socket("18.111.75.234", 8700);
+		dataOutputStream = new DataOutputStream(sendsocket.getOutputStream()); 
+		dataOutputStream.writeUTF("fen"); 
+		}
+		
+		catch (UnknownHostException e) {
+		// TODO Auto-generated catch block
+		    e.printStackTrace();
+		} 
+		catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+		finally{
+		    if (sendsocket != null){
+		        try {
+		            sendsocket.close();
+		        } 
+		        catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		    }
+		}	
+	}
+
+
+
+
+	public static void main(String[] args){
+		MyServer ms = new MyServer();
+		ms.startServer();
+	}
+
 }
